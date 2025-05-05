aa9da63033164e3eb304c17c364dc5c68e827be9
unknown <emmanueladesipe01@gmail.com>
2025-05-04 22:28:59 -0700

    minor adjustment to challenge 7 input field

--- src/challenges/07-Cells/index.svelte
+++ src/challenges/07-Cells/index.svelte
@@ -136,7 +136,7 @@
             input{
                 background: none;
                 outline: none;
-                width: 100%;
+                height: 100%;
                 padding: 0px;
             }
         }
